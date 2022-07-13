2b0d20033acc9df7e5a7f6159e868c674d65b422
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-07-13 14:46:14 +0100

    car js file & main js canvas add ons

---
+++ main.js
@@ -0,0 +1,9 @@
+const { Card } = require("@material-ui/core");
+
+const canvas = document.getElementById("myCanvas");
+canvas.height = window.innerHeight;
+canvas.width = 200;
+
+const ctx = canvas.getContext("2d");
+const car = new Card(100, 100, 30, 50);
+car.draw(ctx);
