8f1a41d8a2106a5ed645cc9c8b2a95e14764c77d
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-07-14 16:18:23 +0100

    Controls onkeyup added

--- controls.js
+++ controls.js
@@ -24,6 +24,24 @@ class Controls{
                     this.reverse = true;
                     break;
             }
+            console.table(this);
+        }
+        document.onkeyup = (event) => {
+            switch (event.key) {
+                case "ArrowLeft":
+                    this.left = false;
+                    break;
+                case "ArrowRight":
+                    this.right = false;
+                    break;
+                case "ArrowUp":
+                    this.forward = false;
+                    break;
+                case "ArrowDown":
+                    this.reverse = false;
+                    break;
+            }
+            console.table(this);
         }
     }
 }
