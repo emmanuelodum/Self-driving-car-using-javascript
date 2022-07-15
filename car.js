dbd4ef71dc17ca839a4fb055fcb45d5cab123203
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-07-15 20:22:57 +0100

    Rotate & translate

--- car.js
+++ car.js
@@ -46,13 +46,18 @@ class Car{
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
         ctx.beginPath();
         ctx.rect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
+            - this.width / 2,
+            - this.height / 2,
             this.width,
             this.height,
         );
         ctx.fill();
+
+        ctx.restore();
     }
 }
