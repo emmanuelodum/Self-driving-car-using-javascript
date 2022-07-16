b9368f6c79929d3b6add0831fe6846e9593269e6
Emmanuel Odum <emmanuelodum48@gmail.com>
2022-07-16 22:02:39 +0100

    Road ctx draw

--- road.js
+++ road.js
@@ -10,6 +10,21 @@ class Road{
         const infinity = 1000000;
         this.top = -infinity;
         this.bottom = infinity;
-        
+
+    }
+
+    draw(ctx) {
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = "white";
+
+        ctx.beginPath();
+        ctx.moveTo(this.left, this.top);
+        ctx.lineTo(this.left, this.bottom);
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(this.right, this.top);
+        ctx.lineTo(this.right, this.bottom);
+        ctx.stroke();
     }
 }
